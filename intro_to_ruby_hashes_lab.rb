dde6c0984a33868d286e74321c9b655f8d205271
Colin Alexander <chalexander@me.com>
2019-08-20 00:52:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,8 @@ def monopoly_with_third_tier
   monopoly[:railroads][:names] = {
     reading_railroad: {},
     pennsylvania_railroad: {},
-    pennsylvania_railroad: {},
-    pennsylvania_railroad: {}
+    b_and_o_railroad: {},
+    shortline_railroad: {}
   }
   return monopoly
 end
