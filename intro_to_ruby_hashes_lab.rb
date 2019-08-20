95e58da570b03f27923012268ece9fa0ca45437a
Colin Alexander <chalexander@me.com>
2019-08-20 00:58:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,5 +40,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly = monopoly_with_second_tier
+  monopoly = monopoly_with_third_tier
+  
+  return monopoly
 end
